d2cc26ab74d5c74b3a30637e4b1231cf519de250
rodion3000 <rodion.papizh@mail.ru>
2022-05-25 10:32:27 +0500

    Добавьте файлы проекта.

---
+++ unit8.6.1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+class FileCleaner
+{
+    public static void Main()
+    {
+        if (Directory.Exists(@"\Users\ggg"))
+            return;
+        try
+        {
+
+            var root = new DirectoryInfo(@"\Users\ggg");
+
+            foreach (var Director in root.GetDirectories())
+            {
+                if (DateTime.Now - Director.LastWriteTime >= TimeSpan.FromMinutes(30))
+                {
+                    try { Director.Delete(); }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+            foreach (var files in root.GetFiles())
+            {
+                if (DateTime.Now - files.LastWriteTime <= TimeSpan.FromMinutes(30))
+                {
+                    try
+                    { files.Delete(); }
+                    catch (Exception ex)
+                    { Console.WriteLine(ex.Message); }
+
+                }
+            }
+
+
+
+        }
+        catch (Exception ex)
+        {
+
+        }
+    }
+
+}
